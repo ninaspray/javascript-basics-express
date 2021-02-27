9b6ced84043b39c87a57c5820c79da3703fdc084
Nina Spray <ninaspray@gmail.com>
2021-02-27 14:32:05 +0000

    wrong files in lib

--- src/app.js
+++ src/app.js
@@ -2,4 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.get('/strings/hello/world', (request, response) => {
+  response.status(200).send({ result: 'Hello, world!' });
+});
+
+app.get('GET /upper/{string}', (request, response) => {
+  response.status(200).send({ result: 'HELLO' });
+});
+
 module.exports = app;
